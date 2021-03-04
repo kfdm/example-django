fa7041892370ff5bd4dfbef9eb564ea05857341c
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-03-04 19:52:58 +0900

    Fix project layout

--- example/standalone/settings.py
+++ example/standalone/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'standalone.urls'
+ROOT_URLCONF = 'example.standalone.urls'
 
 TEMPLATES = [
     {
